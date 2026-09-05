77ad7dd6491b13a58146ee5e5b0558bf008bd44f
andersondinizrota885-ai <andersondinizrota885@gmail.com>
2026-09-05 16:14:43 -0300

    Update criar-jogo.js

--- api/criar-jogo.js
+++ api/criar-jogo.js
@@ -57,7 +57,7 @@ Não coloque texto antes ou depois do JSON.
     console.log("Iniciando chamada para Gemini...");
 
     const resposta = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
+      https://generativelanguage.googleapis.com/v1beta/models/gemini-3.6-flash:generateContent
       {
         method: "POST",
         headers: {
